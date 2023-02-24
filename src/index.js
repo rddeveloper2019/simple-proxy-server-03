61a056099b2eb7a3f92509e904626910d14b2985
rddeveloper2019 <fatzey2010@gmail.com>
2023-02-24 23:23:14 +0300

    Test route added

--- src/index.js
+++ src/index.js
@@ -6,26 +6,30 @@ const defaultsObj=require('./defaultsObj.js')
 const app = express();
 let collectedCookies=require('./collectedCookies.js');
 
-var corsOptions = {
-  origin: 'https://test03.rshb.ru',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// var corsOptions = {
+//   origin: 'https://test03.rshb.ru',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
-
-app.use(cookieParser())
-app.use((req, res, next)=>{
-    // Cookies that have not been signed
-    console.log('Cookies: ', req.cookies)
-   if( Object.keys(req.cookies).length>0) {
-     collectedCookies = {...collectedCookies, ...req.cookies}
-   } else {
-    Object.entries(collectedCookies).forEach((name,value)=>{
-      console.log(name, value)
-      req.cookies[name] = value
-    })
-   }
-  next()
+app.get('/test', (req, res)=>{
+  res.status(200);
+  res.send('proxy-server-03 SUCCESS');
 })
+
+// app.use(cookieParser())
+// app.use((req, res, next)=>{
+//     // Cookies that have not been signed
+//     console.log('Cookies: ', req.cookies)
+//    if( Object.keys(req.cookies).length>0) {
+//      collectedCookies = {...collectedCookies, ...req.cookies}
+//    } else {
+//     Object.entries(collectedCookies).forEach((name,value)=>{
+//       console.log(name, value)
+//       req.cookies[name] = value
+//     })
+//    }
+//   next()
+// })
 app.use(cors());
 app.use('/',  createProxyMiddleware({ target: 'https://test03.rshb.ru', changeOrigin: true }));
 app.use(cors(corsOptions));
