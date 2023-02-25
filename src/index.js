60feb90f42485fa590fccd1fc47dfee72c048d3e
rddeveloper2019 <fatzey2010@gmail.com>
2023-02-25 11:05:50 +0300

    Test03 test04 routes

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ var proxy = require('express-http-proxy');
 
 app.get('/test', (req, res)=>{
   res.status(200);
-  res.send('proxy-server-03 SUCCESS');
+  res.send('<p>https://simple-proxy-server-03.onrender.com/test03</p><p>https://simple-proxy-server-03.onrender.com/test04</p>');
 })
 
 app.use(cors());
-app.use(proxy('https://test03.rshb.ru'));
-
+app.use('/test03', proxy('https://test03.rshb.ru'));
+app.use('/test04', proxy('https://test04.rshb.ru'));
 
 app.listen(3000, ()=> {
   console.log('http://localhost:3000')
