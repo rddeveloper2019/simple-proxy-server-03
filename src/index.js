fa1bd73a2daaa30a27766555af85b8c9e425327f
rddeveloper2019 <fatzey2010@gmail.com>
2023-02-25 14:12:36 +0300

    Исправлено на один маршрут тест 03

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ var proxy = require('express-http-proxy');
 
 app.get('/test', (req, res)=>{
   res.status(200);
-  res.send('<p>https://simple-proxy-server-03.onrender.com/test03</p><p>https://simple-proxy-server-03.onrender.com/test04</p>');
+  res.send('<p>https://simple-proxy-server-03.onrender.com</p>');
 })
 
 app.use(cors());
-app.use('/test03', proxy('https://test03.rshb.ru'));
-app.use('/test04', proxy('https://test04.rshb.ru'));
+app.use('/', proxy('https://test03.rshb.ru'));
+
 
 app.listen(3000, ()=> {
   console.log('http://localhost:3000')
